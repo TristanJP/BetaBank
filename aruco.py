815a7a914a47202f1271c0b4eac418d582693241
TristanJP <trisperk@hotmail.com>
2019-10-29 20:21:18 +0000

    aruco

--- aruco.py
+++ aruco.py
@@ -1,31 +1,31 @@
 import cv2
+from cv2 import aruco
 import numpy as np
+import matplotlib as mpl
 from matplotlib import pyplot as plt
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
-img = cv2.imread("thai.jpg")
-alpha = 0.4
-
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     frame = cv2.flip(frame,1)
 
-    added_image = cv2.addWeighted(frame[100:640,100:640,:],alpha,img[0:540,0:540,:],1-alpha,0)
-
-    frame[100:640,100:640,:] = added_image
-
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
+    # Post Processing
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    parameters =  aruco.DetectorParameters_create()
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+    frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
 
     # Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+
 
 # When everything done, release the capture
 cap.release()
