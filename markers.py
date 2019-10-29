7556971d843954b080d87f0b75770df473768992
TristanJP <trisperk@hotmail.com>
2019-10-29 19:06:50 +0000

    mpl

--- markers.py
+++ markers.py
@@ -1,6 +1,7 @@
 import cv2
 from cv2 import aruco
 import numpy as np
+import matplotlib as mpl
 from matplotlib import pyplot as plt
 
 aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
@@ -9,3 +10,11 @@ fig = plt.figure()
 nx = 4
 ny = 3
 
+for i in range(1, nx*ny+1):
+    ax = fig.add_subplot(ny,nx, i)
+    img = aruco.drawMarker(aruco_dict,i, 700)
+    plt.imshow(img, cmap = mpl.cm.gray, interpolation = "nearest")
+    ax.axis("off")
+
+plt.savefig("_data/markers.pdf")
+plt.show()
