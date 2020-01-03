bf2cf3080355c03bb03b871486549f3b7443a238
TristanJP <trisperk@hotmail.com>
2020-01-03 22:25:21 +0000

    made object_3d call calibrate

--- object_3d.py
+++ object_3d.py
@@ -9,15 +9,17 @@ from calibrate import Calibrate
 class Object_3d:
 
     CALIBRATION_IMAGE_PATH: str
+    search_aruco_dict: None
     cal = None
 
-    def __init__(self, path):
+    def __init__(self, path, search_aruco_dict):
         self.CALIBRATION_IMAGE_PATH = path
+        self.search_aruco_dict = search_aruco_dict
 
-        self.cal = Calibrate(self.CALIBRATION_IMAGE_PATH)
+        self.cal = Calibrate(self.CALIBRATION_IMAGE_PATH, self.search_aruco_dict)
 
 if __name__ == "__main__":
-    object_3d = Object_3d("calibration_images")
+    object_3d = Object_3d(path="calibration_images", search_aruco_dict=aruco.DICT_6X6_250)
     object_3d.cal.calibrate_camera()
 
     object_3d.cal.show_pose()
