8b70c9c4fa0f578684b9156d8702975fdb760444
TristanJP <trisperk@hotmail.com>
2020-02-21 15:50:18 +0000

    Initial frame analyser

--- frame_analyser.py
+++ frame_analyser.py
@@ -9,29 +9,30 @@ class Frame_Analyser:
     CALIBRATION_IMAGE_PATH: str
     search_aruco_dict: None
     cal = None
+    detection = None
 
-    def __init__(self, frame, path="calibration_images", search_aruco_dict=cv2.aruco.DICT_6X6_250):
+    def __init__(self, path="calibration_images", search_aruco_dict=cv2.aruco.DICT_6X6_250):
         self.CALIBRATION_IMAGE_PATH = path
         self.search_aruco_dict = cv2.aruco.getPredefinedDictionary(search_aruco_dict)
 
         self.cal = Calibrate(path, search_aruco_dict)
+        calibration_data = self.cal.calibrate_camera()
+        self.detection = Detection(calibration_data)
 
+    def analyse_video(self, video_path):
+        print("test")
+
+    def anaylse_frame(self, frame_path):
+        frame = cv2.imread(frame_path)
+        rvecs, tvecs, objPoints = self.detection.get_markers_in_frame(frame, self.search_aruco_dict)
+
+        return rvecs, tvecs, objPoints
 
 if __name__ == "__main__":
 
-    cam = Camera()
-    cam.start()
+    frame_analyser = Frame_Analyser()
+    frame_analyser.anaylse_frame("test_images/capture_10.png")
 
-    while (True):
-        frame = cam.current_frame
-        cv2.imshow("frame", frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cam.release_camera()
-            break
-    
-    #frame_analyser = Frame_Analyser()
-    #detection = Detection()
     #detection.get_markers_in_frame()
     #ret, camera_matrix, distortion_coefficients0, rotation_vectors, translation_vectors = 
     #detection.cal.calibrate_camera()
