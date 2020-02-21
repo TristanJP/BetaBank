0da0fa0312bba03f06b401cadc8ca763c052a931
TristanJP <trisperk@hotmail.com>
2020-02-21 15:48:55 +0000

    Rename and fix detection

--- Detection.py
+++ Detection.py
@@ -4,6 +4,9 @@ from camera import Camera
 
 class Detection():
 
+    def __init__(self, calibration_data):
+        self.calibration_data = calibration_data
+
     def get_markers_in_frame(self, frame, search_aruco_dict):
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         parameters =  aruco.DetectorParameters_create()
@@ -17,11 +20,10 @@ class Detection():
             cv2.cornerSubPix(gray_frame, corner, winSize = (3,3), zeroZone = (-1,-1), criteria = criteria)
 
         size_of_marker =  0.0125 # side length of the marker in meters
-        length_of_axis = 0.025
 
-        rvecs, tvecs, objPoints = aruco.estimatePoseSingleMarkers(corners, size_of_marker , self.camera_matrix, self.distortion_coefficients0)
+        rvecs, tvecs, objPoints = aruco.estimatePoseSingleMarkers(corners, 
+                                                                  size_of_marker,
+                                                                  self.calibration_data["cam_mtx"],
+                                                                  self.calibration_data["dist_coef"])
 
-        print(rvecs)
-        print(self.camera.calibration_data("rvecs"))
-
-        
+        return rvecs, tvecs, objPoints
