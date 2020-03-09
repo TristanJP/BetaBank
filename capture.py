8744cdc1c3ced0ac4be6bb475d5176dce53b70c5
TristanJP <trisperk@hotmail.com>
2020-03-09 17:08:15 +0000

    add video cap

--- capture.py
+++ capture.py
@@ -35,10 +35,42 @@ class Capture:
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 cam.release_camera()
                 break
+    
+    def take_video(self, grayscale, video_folder, filename, search_aruco_dict=cv2.aruco.DICT_6X6_250):
+
+        out = cv2.VideoWriter(f"{video_folder}/{filename}",cv2.VideoWriter_fourcc('M','J','P','G'), 10, (1280, 720))
+
+        search_aruco_dict = cv2.aruco.getPredefinedDictionary(search_aruco_dict)
+        i = 0
+
+        while True:
+            # Capture frame-by-frame
+            frame = self.cam.current_frame
+            ret = self.cam.successful_read
+            if grayscale:
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            else:
+                gray = frame
+
+            # get corners
+            corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, search_aruco_dict)
+            frame_markers = aruco.drawDetectedMarkers(gray.copy(), corners, ids)
+
+            # Save frame
+            out.write(frame_markers)
+
+            cv2.imshow('frame', gray)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                self.cam.release_camera()
+                out.release()
+                break
 
 
 if __name__ == "__main__":
     
     cap = Capture()
 
-    cap.take_pictures(False, "test_images")
+    #cap.take_pictures(False, "test_images")
+
+    cap.take_video(False, "test_videos", "test.avi")
