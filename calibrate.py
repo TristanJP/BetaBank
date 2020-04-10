b327b8d5abbe0335a6296cc265b4e67744a6e503
TristanJP <trisperk@hotmail.com>
2020-04-10 19:08:19 +0100

    tidy calibration

--- calibrate.py
+++ calibrate.py
@@ -22,48 +22,35 @@ class Calibrate:
         name = f"{path}.yaml"
         for root, dirs, files in os.walk(path):
             if name in files:
-                self.CALIBRATION_STORED_DATA = os.path.join(root, name)      
-
-    # OLD
-    def capture_camera(self):
-        cap = cv2.VideoCapture(0)
-        cap.set(3, 1280)
-        cap.set(4, 720)
-        return cap
-
-    # OLD
-    def release_camera(self, cap):
-        cap.release()
-        cv2.destroyAllWindows()
+                self.CALIBRATION_STORED_DATA = os.path.join(root, name)
 
     def get_calibration_images(self):
         fnames = glob(f"{self.CALIBRATION_IMAGE_PATH}/*.png")
         return fnames
 
     def read_chessboards(self):
-        # Charuco base pose estimation.
+        # Charuco based pose estimation.
         images = self.get_calibration_images()
 
-        #print("POSE ESTIMATION STARTS:")
         all_corners = []
         all_ids = []
         decimator = 0
-        # SUB PIXEL CORNER DETECTION CRITERION
+
+        # SUB PIXEL CORNER DETECTION CRITERIA
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.00001)
 
-        for im in images:
-            print(f"=> Processing image {im}")
-            frame = cv2.imread(im)
+        for image in images:
+            print(f"=> Processing image {image}")
+            frame = cv2.imread(image)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, self.calibrate_aruco_dict)
 
-            if len(corners)>0:
+            if len(corners) > 0:
+
                 # SUB PIXEL DETECTION
                 for corner in corners:
-                    cv2.cornerSubPix(gray, corner,
-                                    winSize = (3,3),
-                                    zeroZone = (-1,-1),
-                                    criteria = criteria)
+                    cv2.cornerSubPix(gray, corner,  winSize = (3,3),  zeroZone = (-1,-1), criteria = criteria)
+
                 res2 = cv2.aruco.interpolateCornersCharuco(corners,ids,gray,self.board)
                 if res2[1] is not None and res2[2] is not None and len(res2[1])>3 and decimator%1==0:
                     all_corners.append(res2[1])
@@ -78,59 +65,53 @@ class Calibrate:
         print("CAMERA CALIBRATION:")
         if self.CALIBRATION_STORED_DATA != None:
             print(f"  Found calibration data: {self.CALIBRATION_STORED_DATA}, loading...")
-            self.calibration_data = self.loadCoefficients(self.CALIBRATION_STORED_DATA)
+            self.calibration_data = self.load_coefficients(self.CALIBRATION_STORED_DATA)
         else:
             print("Generating new calibration data...")
             # Calibrates the camera using the detected corners.
             all_corners, all_ids, imsize = self.read_chessboards()
 
-            cameraMatrixInit = np.array([[ 1000.,    0., imsize[0]/2.],
-                                        [    0., 1000., imsize[1]/2.],
-                                        [    0.,    0.,           1.]])
+            camera_matrix_init = np.array(
+                [
+                    [ 1000., 0., imsize[0]/2.],
+                    [ 0., 1000., imsize[1]/2.],
+                    [ 0., 0., 1.]
+                ]
+            )
 
-            distCoeffsInit = np.zeros((5,1))
+            dist_coeffs_init = np.zeros((5,1))
             flags = (cv2.CALIB_USE_INTRINSIC_GUESS + cv2.CALIB_RATIONAL_MODEL + cv2.CALIB_FIX_ASPECT_RATIO)
             #flags = (cv2.CALIB_RATIONAL_MODEL)
             (ret, camera_matrix, distortion_coefficients,
             rotation_vectors, translation_vectors,
-            stdDeviationsIntrinsics, stdDeviationsExtrinsics,
-            perViewErrors) = cv2.aruco.calibrateCameraCharucoExtended(
+            standard_deviations_intrinsics, standard_deviations_extrinsics,
+            per_view_errors) = cv2.aruco.calibrateCameraCharucoExtended(
                             charucoCorners=all_corners,
                             charucoIds=all_ids,
                             board=self.board,
                             imageSize=imsize,
-                            cameraMatrix=cameraMatrixInit,
-                            distCoeffs=distCoeffsInit,
+                            cameraMatrix=camera_matrix_init,
+                            distCoeffs=dist_coeffs_init,
                             flags=flags,
                             criteria=(cv2.TERM_CRITERIA_EPS & cv2.TERM_CRITERIA_COUNT, 10000, 1e-9))
 
             self.calibration_data = {"ret": ret, "cam_mtx": camera_matrix, "dist_coef": distortion_coefficients, "cam_rvecs": rotation_vectors, "cam_tvecs": translation_vectors}
-            self.saveCoefficients(f"{self.CALIBRATION_IMAGE_PATH}/{self.CALIBRATION_IMAGE_PATH}.yaml", camera_matrix, distortion_coefficients)
+            self.save_coefficients(f"{self.CALIBRATION_IMAGE_PATH}/{self.CALIBRATION_IMAGE_PATH}.yaml", camera_matrix, distortion_coefficients)
 
         print("DONE CALIBRATION")
         return self.calibration_data
 
-    def saveCoefficients(self, path, mtx, dist):
-        cv_file = cv2.FileStorage(f"{path}", cv2.FILE_STORAGE_WRITE)
-        cv_file.write("camera_matrix", mtx)
-        cv_file.write("dist_coeff", dist)
-        # note you *release* you don't close() a FileStorage object
-        cv_file.release()
+    def save_coefficients(self, path, mtx, dist):
+        mc_file = cv2.FileStorage(f"{path}", cv2.FILE_STORAGE_WRITE)
+        mc_file.write("camera_matrix", mtx)
+        mc_file.write("dist_coeff", dist)
+        mc_file.release()
 
-    def loadCoefficients(self, path):
-        # FILE_STORAGE_READ
-        cv_file = cv2.FileStorage(f"{path}", cv2.FILE_STORAGE_READ)
-
-        # note we also have to specify the type to retrieve other wise we only get a
-        # FileNode object back instead of a matrix
-        camera_matrix = cv_file.getNode("camera_matrix").mat()
-        dist_matrix = cv_file.getNode("dist_coeff").mat()
-
-        # Debug: print the values
-        # print("camera_matrix : ", camera_matrix.tolist())
-        # print("dist_matrix : ", dist_matrix.tolist())
-
-        cv_file.release()
+    def load_coefficients(self, path):
+        mc_file = cv2.FileStorage(f"{path}", cv2.FILE_STORAGE_READ)
+        camera_matrix = mc_file.getNode("camera_matrix").mat()
+        dist_matrix = mc_file.getNode("dist_coeff").mat()
+        mc_file.release()
         return {"cam_mtx": camera_matrix, "dist_coef": dist_matrix}
 
     # show images
@@ -149,64 +130,6 @@ class Calibrate:
         img_undist = cv2.undistort(frame, self.calibration_data["cam_mtx"], self.calibration_data["dist_coef"], None)
         return [frame, img_undist]
 
-    def show_pose_for_image(self, image):
-        frame = cv2.imread(f"{self.CALIBRATION_IMAGE_PATH}/{image}")
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        parameters =  aruco.DetectorParameters_create()
-        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, self.search_aruco_dict,
-                                                            parameters=parameters)
-        # SUB PIXEL DETECTION
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.0001)
-        for corner in corners:
-            cv2.cornerSubPix(gray, corner, winSize = (3,3), zeroZone = (-1,-1), criteria = criteria)
-
-        frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
-
-        size_of_marker =  0.045 # side lenght of the marker in meters
-        rvecs, tvecs, objPoints = aruco.estimatePoseSingleMarkers(corners, size_of_marker , self.camera_matrix, self.distortion_coefficients)
-
-        length_of_axis = 0.1
-        imaxis = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
-
-        for i in range(len(tvecs)):
-            imaxis = aruco.drawAxis(imaxis, self.camera_matrix, self.distortion_coefficients, rvecs[i], tvecs[i], length_of_axis)
-
-        plt.figure()
-        plt.imshow(imaxis)
-        plt.grid()
-        plt.show()
-
-    # OLD
-    def show_pose(self):
-        cap = self.capture_camera()
-
-        while True:
-            ret, frame = cap.read()
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            parameters =  aruco.DetectorParameters_create()
-            corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, self.search_aruco_dict,
-                                                                parameters=parameters)
-            # SUB PIXEL DETECTION
-            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.0001)
-            for corner in corners:
-                cv2.cornerSubPix(gray, corner, winSize = (3,3), zeroZone = (-1,-1), criteria = criteria)
-
-            size_of_marker =  0.0125 # side length of the marker in meters
-            rvecs, tvecs, objPoints = aruco.estimatePoseSingleMarkers(corners, size_of_marker , self.camera_matrix, self.distortion_coefficients)
-
-            length_of_axis = 0.025
-            imaxis = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
-
-            if tvecs is not None:
-                for i in range(len(tvecs)):
-                    imaxis = aruco.drawAxis(frame, self.camera_matrix, self.distortion_coefficients, rvecs[i], tvecs[i], length_of_axis)
-
-            cv2.imshow("frame", imaxis)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                self.release_camera(cap)
-                break
-
-
 # =================================
 # Run Calibration
 
@@ -219,9 +142,3 @@ if __name__ == "__main__":
 
     undist = cal.get_undistorted_image(f"{cal.CALIBRATION_IMAGE_PATH}/capture_7.png")
     cal.show_images(undist)
-
-    #cal.show_pose_for_image("capture_1.png")
-
-    #cal.show_pose()
-
-
